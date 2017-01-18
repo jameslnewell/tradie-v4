6583b9150699d902fec501833bc05e080eaa1844
James Newell <james@digitaledgeit.com.au>
2017-01-19 03:08:04 +1100

    fix bug with reference dll plugin

--- packages/tradie-webpack-config/lib/ReferenceDLLPlugin.js
+++ packages/tradie-webpack-config/lib/ReferenceDLLPlugin.js
@@ -2,19 +2,27 @@
 const webpack = require('webpack');
 
 /**
- * Load the vendor manifest on demand without watching it (which results in re-builds because the timestamp is so recent)
+ * Load the vendor manifest as the reference compiler is compiling (instead of when the reference compiler is instanciated)
+ * and don't let webpack watch it (which results in re-builds because the timestamp is so new)
  */
 class ReferenceDLLPlugin {
 
   constructor(options) {
+    this.loaded = false;
     this.manifest = options.manifest;
   }
 
   apply(compiler) {
-    compiler.apply(new webpack.DllReferencePlugin({
-      context: compiler.options.context,
-      manifest: require(this.manifest)
-    }));
+    compiler.plugin(['run', 'watch-run'], (compilerOrWatcher, callback) => {
+      if (!this.loaded) {
+        compiler.apply(new webpack.DllReferencePlugin({
+          context: compiler.options.context,
+          manifest: require(this.manifest)
+        }));
+      }
+      this.loaded = true;
+      callback();
+    });
   }
 
 }
