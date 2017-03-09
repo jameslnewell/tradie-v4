8fa6c1811973a92082d6e7fbe8ea983a2da1a6f7
James Newell <james@digitaledgeit.com.au>
2017-03-09 23:08:51 +1100

    added jest for testing

--- packages/tradie-webpack-scripts/lib/test.js
+++ packages/tradie-webpack-scripts/lib/test.js
@@ -1,7 +1,6 @@
 'use strict';
-const wfe = require('wait-for-event');
-const Bundler = require('./util/Bundler');
-const BuildReporter = require('./util/BuildReporter');
+const chalk = require('chalk');
+const jest = require('jest');
 
 /**
  * Run webpack on multiple bundles and display the results
@@ -9,45 +8,22 @@ const BuildReporter = require('./util/BuildReporter');
  * @param {string}  [options.root]
  * @param {boolean} [options.debug=false]
  * @param {boolean} [options.watch=false]
- * @param {object}  options.webpack
+ * @param {object}  options.jest
  * @returns {Promise.<null>}
  */
 module.exports = options => new Promise((resolve, reject) => {
+  const watch = options.watch;
+  const config = options.jest;
 
-  //create the test bundler
-  const bundler = new Bundler(options.webpack, {
-    name: 'test',
-    watch: options.watch,
-    virtual: true
-  });
+  if (!config) {
+    console.error(chalk.red('Tradie template does not provide a test configuration.'));
+    return reject(1);
+  }
 
-  //create the reporter
-  const reporter = new BuildReporter({
-    debug: options.debug,
-    bundlers: [bundler]
-  });
-
-  //start the bundler
-  bundler.start();
-
-  //stop all the things when the user wants to exit
-  process.on('SIGINT', () => {
-    bundler.stop();
-  });
-
-  //wait for the bundler to close before resolving or rejecting
-  return new Promise((resolve, reject) => {
-    wfe.waitForAll('stopped', [bundler], errors => {
-      setImmediate(() => { //HACK: wait for build-reporter
-        if (errors.length) {
-          reject(errors);
-        } else if (reporter.errors.length) {
-          reject();
-        } else {
-          resolve();
-        }
-      });
-    });
-  });
+  jest.run([
+    watch ? '--watch' : '',
+    '--config',
+    JSON.stringify(config)
+  ]);
 
 });
