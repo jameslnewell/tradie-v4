e44127177fd8b27014070e7b9eda81a48bfae3de
James Newell <j.newell@nib.com.au>
2017-03-10 15:28:38 +1100

    prioritised todos

--- packages/tradie-template-react-app/config/lib/todo.js
+++ packages/tradie-template-react-app/config/lib/todo.js
@@ -1,9 +1,9 @@
 
 //TODO: list assets in file for preload and fingerprinting // config.plugins.push(new AssetManifestPlugin());
-//TODO: baseurl
-//TODO: service worker
-//TODO: env vars
 //TODO: eslint, stylelint
+//TODO: baseurl
+//TODO: env vars
+//TODO: service worker
 //TODO: remove CSS loader?
 
 
