cc61982d56a845e48eabeb64b044950616220002
James Newell <j.newell@nib.com.au>
2017-03-13 16:35:36 +1100

    fix linting

--- packages/tradie-template-react-app/config/lib/getEslintClientConfig.js
+++ packages/tradie-template-react-app/config/lib/getEslintClientConfig.js
@@ -1,4 +1,14 @@
 'use strict';
 const getEslintCommonConfig = require('./getEslintCommonConfig');
 
-module.exports = getEslintCommonConfig;
+module.exports = options => {
+  const config = getEslintCommonConfig(options);
+  return Object.assign({}, config, {
+    env: {
+      browser: true
+    },
+    rules: {
+    },
+    extends: [].concat(config.extends)
+  });
+};
--- packages/tradie-template-react-app/config/lib/getEslintServerConfig.js
+++ packages/tradie-template-react-app/config/lib/getEslintServerConfig.js
@@ -4,9 +4,11 @@ const getEslintCommonConfig = require('./getEslintCommonConfig');
 module.exports = options => {
   const config = getEslintCommonConfig(options);
   return Object.assign({}, config, {
+    env: {
+      node: true
+    },
     rules: {
-      'no-console': 'off',
-      'indent': 'off' //FIXME: where is this coming from?
+      'no-console': 'off'
     },
     extends: [].concat(config.extends)
   });
