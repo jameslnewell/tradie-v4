930370c8bd70ae0384a9c63585abfe7b24de3f2a
James Newell <james@digitaledgeit.com.au>
2017-04-15 07:36:11 +1000

    moved server stuff from tradie to the templates

--- packages/tradie-template-react-static-site/config/serve.js
+++ packages/tradie-template-react-static-site/config/serve.js
@@ -1,4 +1,6 @@
 'use strict';
+const serveStatic = require('serve-static');
+const getPaths = require('./lib/getPaths');
 const getSiteMetadata = require('./lib/getSiteMetadata');
 const getWebpackVendorConfig = require('./lib/getWebpackVendorConfig');
 const getWebpackClientConfig = require('./lib/getWebpackClientConfig');
@@ -10,6 +12,8 @@ module.exports = options => {
   const optimize = false;
   const manifest = [];
 
+  const paths = getPaths(root);
+  
   return getSiteMetadata(root)
     .then(metadata => {
       return {
@@ -18,7 +22,8 @@ module.exports = options => {
           vendor: getWebpackVendorConfig({root, optimize, manifest}),
           client: getWebpackClientConfig({root, optimize, metadata, manifest}),
           build: getWebpackBuildConfig({root, optimize, metadata, manifest})
-        }
+        },
+        onServerStart: server => {console.log(paths.dest); server.use(serveStatic(paths.dest))}
       };
     })
   ;
