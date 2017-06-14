45c90b486c19587a6e837ff0fac63a0de05a2e1c
James Newell <james@digitaledgeit.com.au>
2017-06-15 01:08:04 +1000

    fix: removed the builder, split the reporting into tradie-utils-reporter

--- packages/tradie-scripts-node-package/scripts/build.js
+++ packages/tradie-scripts-node-package/scripts/build.js
@@ -1,5 +1,11 @@
 const path = require('path');
-const Builder = require('../lib/Builder');
+
+const chalk = require('chalk');
+const Files = require('tradie-utils-file');
+const Linter = require('tradie-utils-eslint').Linter;
+const Transpiler = require('tradie-utils-babel');
+const TypeChecker = require('tradie-utils-flow');
+const Reporter = require('tradie-utils-reporter').default;
 
 module.exports = function(options) {
   const root = options.root;
@@ -10,20 +16,85 @@ module.exports = function(options) {
   const babel = options.babel;
   const eslint = options.eslint;
   const watch = options.watch;
-  const builder = new Builder({
-    root,
-    src,
-    dest,
-    include,
-    exclude,
-    babel,
-    eslint,
-    watch
+
+  files = new Files(src, {
+    watch: watch,
+    include: include,
+    exclude: exclude
   });
+
+  linter = new Linter(src, eslint);
+  transpiler = new Transpiler(src, dest, babel);
+  typechecker = new TypeChecker(root, src);
+  reporter = new Reporter({watching: watch});
+
+  const buildFile = file => {
+    return Promise.all([
+      linter.lint(file).then(result => {
+        if (result.error) {
+          reporter.error(file, result.error); //TODO: prioritise less than transpiler error
+        }
+        //TODO: warnings
+      }),
+
+      transpiler.transpile(file).catch(error => reporter.error(file, error))
+    ]);
+  };
+
+  const deleteFile = file => {
+    return del(transpiler.destFile(file)).catch(error =>
+      reporter.error(file, error)
+    );
+  };
+
+  const checkAllFiles = () => {
+    return typechecker
+      .check()
+      .then(errors => {
+        //squash all flow errors into a single error
+        const flatErrorsByFile = {};
+        errors.forEach(error => {
+          if (!flatErrorsByFile[error.file]) {
+            flatErrorsByFile[error.file] = [];
+          }
+          flatErrorsByFile[error.file].push(`${error.message}`);
+        });
+
+        //add the errors
+        Object.keys(flatErrorsByFile).forEach(file =>
+          reporter.error(file, flatErrorsByFile[file].join('\n'))
+        );
+      })
+      .catch(error => reporter.fatalError(error));
+  };
+
+  //run the initial build
+  reporter.started();
+  files
+    .list()
+    .then(list => Promise.all(list.map(file => buildFile(file))))
+    .then(() => reporter.finished(), error => reporter.fatalError(error));
+
+  if (watch) {
+    files.on('add', file => {
+      reporter.started();
+      buildFile(file).then(() => reporter.finished());
+    });
+
+    files.on('change', file => {
+      reporter.started();
+      buildFile(file).then(() => reporter.finished());
+    });
+
+    files.on('unlink', file => {
+      reporter.started();
+      deleteFile(file).then(() => reporter.finished());
+    });
+  }
 
   process.on('SIGINT', () => {
-    builder.stop();
+    reporter.stop();
   });
 
-  return builder.start().wait();
+  return reporter.wait();
 };
