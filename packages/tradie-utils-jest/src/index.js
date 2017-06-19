e40f9ecfcd2c0658718ac2285c15e39fe1fd0f31
James Newell <james@digitaledgeit.com.au>
2017-06-19 22:31:48 +1000

    fix: switch to a transpiled library

---
+++ packages/tradie-utils-jest/src/index.js
@@ -0,0 +1,39 @@
+// @flow
+import jest from 'jest';
+import babel from 'babel-core';
+
+type Options = {
+  config: {},
+  watch?: boolean,
+  coverage?: boolean
+};
+
+export default function(options: Options): Promise<void> {
+  const {config, watch = options, coverage = false} = options;
+
+  const args = ['--config', JSON.stringify(config)];
+
+  if (watch) {
+    args.push('--watch');
+  }
+
+  if (coverage) {
+    args.push('--coverage');
+  }
+
+  //TODO: support other args? https://facebook.github.io/jest/docs/cli.html
+
+  return new Promise(() => {
+    //TODO: resolve() when completed. see https://github.com/facebook/jest/issues/3737.
+    jest.run(args);
+  });
+}
+
+export function createBabelTransform(babelOptions: {}) {
+  return {
+    process(src: string, file: string) {
+      return babel.transform(src, Object.assign({filename: file}, babelOptions))
+        .code;
+    }
+  };
+}
